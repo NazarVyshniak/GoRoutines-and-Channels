90cb9bd05c98e13f1ffc0888ae5b0522cb930614
nazar <visnyaknazar@gmail.com>
2025-07-27 13:49:57 +0200

    Completed project

--- Bank account/main.go
+++ Bank account/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+var wg sync.WaitGroup
+
+type Income struct {
+	Source string
+	Amount int
+}
+
+func main() {
+	var bankBalance int
+	var balance sync.Mutex
+
+	fmt.Printf("Initial account balance: $%d.00\n", bankBalance)
+
+	incomes := []Income{
+		{Source: "Main job", Amount: 500},
+		{Source: "Gifts", Amount: 10},
+		{Source: "Part time job", Amount: 50},
+		{Source: "Investments", Amount: 100},
+	}
+
+	wg.Add(len(incomes))
+
+	for i, income := range incomes {
+		go func(i int, income Income) {
+			defer wg.Done()
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+
+				fmt.Printf("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+			}
+		}(i, income)
+	}
+
+	wg.Wait()
+	fmt.Printf("Final balance: $%d.00\n", bankBalance)
 }
