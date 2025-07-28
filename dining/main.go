a3e52b9611ecf29433ef5dac65a6cbbd09c127c8
nazar <visnyaknazar@gmail.com>
2025-07-28 11:45:16 +0200

    Complete code

--- dining/main.go
+++ dining/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -21,17 +22,72 @@ var philosophers = []Philosopher{
 
 var hunger = 3
 var eatTime = 1 * time.Second
-var think = 3 * time.Second
-var sleepTime = 1 * time.Second
+var thinkTime = 3 * time.Second
 
 func main() {
 	fmt.Println("Dining Philosophers Problem")
 	fmt.Println("---------------------------")
+	fmt.Println("The table is empty.")
 
 	dine()
 
+	fmt.Println("The table is empty.")
 }
 
 func dine() {
+	eatTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
 
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	for i := 0; i < len(philosophers); i++ {
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+	seated.Done()
+
+	seated.Wait()
+
+	for i := hunger; i > 0; i-- {
+		if philosopher.leftFork > philosopher.rightFork {
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("%s takes the right fork.\n", philosopher.name)
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("%s takes the left fork.\n", philosopher.name)
+		} else {
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("%s takes the left fork.\n", philosopher.name)
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("%s takes the right fork.\n", philosopher.name)
+		}
+
+		fmt.Printf("%s has both forks and is eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		fmt.Printf("%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		forks[philosopher.leftFork].Unlock()
+		forks[philosopher.rightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", philosopher.name)
+	}
+
+	fmt.Println(philosopher.name, "is satisfied.")
+	fmt.Println(philosopher.name, "left the table.")
 }
