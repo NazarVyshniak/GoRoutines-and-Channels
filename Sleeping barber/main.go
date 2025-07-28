b82c30c097bc54231f3ee42d01fe32d2a54d324f
nazar <visnyaknazar@gmail.com>
2025-07-28 16:59:16 +0200

    First steps

--- Sleeping barber/main.go
+++ Sleeping barber/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1000 * time.Millisecond
+var timeOpen = 10 * time.Second
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
+
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 
 }
